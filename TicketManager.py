764d88afb89ce57eba450f90bf284b6846b6eb2b
xcb3d <xcode3d@gmail.com>
2025-03-04 01:04:22 +0700

    refactor ticket

--- TicketManager.py
+++ TicketManager.py
@@ -1,17 +1,6 @@
 import random
 import datetime
-
-class Ticket:
-    def __init__(self, ticket_id, user_id, train_id, route, schedule, seats, price, payment_status="Đã thanh toán", purchase_time=None):
-        self.ticket_id = ticket_id
-        self.user_id = user_id
-        self.train_id = train_id
-        self.route = route
-        self.schedule = schedule
-        self.seats = seats  # Danh sách các ghế đã đặt
-        self.price = price
-        self.payment_status = payment_status
-        self.purchase_time = purchase_time or datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+from Ticket import Ticket
 
 class TicketManager:
     TICKETS_FILE_PATH = "tickets.txt"
